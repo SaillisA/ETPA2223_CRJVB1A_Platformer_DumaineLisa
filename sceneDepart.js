b6f0b0574b43017753b1e508296d963a5cb670e0
SaillisA <dumaine.lisa04@gmail.com>
2023-05-10 12:57:53 +0200

    Concept art finis

---
+++ sceneDepart.js
@@ -0,0 +1,20 @@
+class sceneDepart extends Phaser.Scene {
+    constructor(){
+        super("scene")
+
+    }
+    init(data){
+        {
+    }
+    }
+    preload(){
+
+    }
+    create(){
+
+    }
+    update(){
+
+    }
+
+}
